2021142466dad80e287336182b86f048af052dc6
YoussefAhmed52 <yousef3012001@gmail.com>
2023-06-18 13:31:51 -0700

    print alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,20 +4,17 @@
  * Description: print alphabet letters except q and e
  * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	char ch = 'a';
-
 	while (ch <= 'z')
 	{
-
-	if
-		(ch == 'e' || ch == 'q');
-	ch++;
-	putchar(ch);
-	ch++;
+		if (ch == 'e' || ch == 'q');
+		ch++;
+		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
 }
-
