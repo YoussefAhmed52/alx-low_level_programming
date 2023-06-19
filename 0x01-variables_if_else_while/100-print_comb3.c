5838d13607a9b75d8f77a5b469a4fd88a5d9fe89
YoussefAhmed52 <yousef3012001@gmail.com>
2023-06-18 21:48:51 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,28 @@
 */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1 = 0, digit2;
 
-	for (digit1 = 0; digit1 <= 8; digit1++)
+	while (digit1 <= 9)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-
-			if (digit1 != 8 || digit2 != 9)
+			if (digit1 != digit2 && digit1 < digit2)
 			{
-				putchar(',');
-				putchar(' ');
-			}
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
 
-			if (digit2 == 9 && digit1 == 9)
-			{
-				putchar('\n');
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			digit2++;
 		}
+		digit1++;
 	}
+	putchar('\n');
 	return (0);
 }
