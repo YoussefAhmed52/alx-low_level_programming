76ea0fa9c3bb68ce27961b6e1db6346ac88bfcc8
YoussefAhmed52 <yousef3012001@gmail.com>
2023-06-18 22:14:08 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,25 +14,24 @@ int main(void)
 
 		while (w <= 99)
 		{
+			if (q != 0 || w != 0)
+			{
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
 			putchar(' ');
 			putchar((w / 10) + '0');
 			putchar((w % 10) + '0');
+			}
 
 			if (q != 99 || w != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-
-			if (w == 99 && q == 99)
-			{
-				putchar('\n');
-			}
 			w++;
 		}
 		q++;
 	}
+	putchar('\n');
 	return (0);
 }
