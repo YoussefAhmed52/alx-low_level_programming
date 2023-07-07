ab40063508d8c53eae3786dcea6db8357aaf14ba
YoussefAhmed52 <yousef3012001@gmail.com>
2023-07-07 02:51:21 -0700

    cmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
  * Return: 1 if true, 0 if false
 */
+
 int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
