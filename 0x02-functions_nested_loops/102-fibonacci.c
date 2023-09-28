10b96ad1e7817c6cf2be290a828fd78d2992e9aa
YoussefAhmed52 <yousef3012001@gmail.com>
2023-09-28 00:31:20 -0700

    F

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,7 @@
  * Description: print the first 50 fibonacci numbers
  *		starting with 1 and 2 followed by a new line
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
