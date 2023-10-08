95b4dac2e3aa692a76536604aaf4885167cf2298
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-07 22:06:52 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,22 +14,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, s1_length, s2_length;
 
+	/*Check if the strings passed are null*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	/*compute the length of the strings*/
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 		;
 
+	/*memory reservation for case 1 & 2*/
+
 	str = malloc(s1_length + n + 1);
 	if (str == NULL)
 		return (NULL);
 
+	/*copy first string into str */
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
+
+	/*copy second string into str */
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
