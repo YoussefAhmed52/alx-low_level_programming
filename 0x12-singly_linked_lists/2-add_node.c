d680b1ab7c041295eae228eaadf43da3c1924e9a
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-19 19:50:32 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 			free(new_head);
 			return (NULL);
 		}
-		new_head->len = _strlen(new_head);
+		new_head->len = _strlen(new_head->str);
 	}
 	new_head->next = *head;
 	*head = new_head;
