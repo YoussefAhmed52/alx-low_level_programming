bc2d93e4b24c4f7bcee8055b291023865fa4c2cc
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-19 18:46:17 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+
+/**
+ * _strlen - length of the string
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (j);
+}
+/**
+ * print_list - print a linked list
+ * @h: pointer to the first node
+ * Return: list size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlen(h->str) ? h->str : "(nil)");
+		h = h->next;
+		j++;
+	}
+	return (j);
+}
