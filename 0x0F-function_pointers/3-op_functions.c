0a6626e50e57d524a7a3cea4de7609fe52baf9ca
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-19 19:47:33 -0700

    0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two inegers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two inegers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two inegers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two inegers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: quotiant
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - divides two inegers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: reminder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
