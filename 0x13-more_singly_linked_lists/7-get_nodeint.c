01cb241e7234d8f58bbed5e69ee5fce6ea401792
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-24 02:50:45 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int k;
 
-	for (node = head, k = 0; node && k < index; node = node->next; k++)
+	for (node = head, k = 0; node && k < index; node = node->next, k++)
 		;
 	return (node);
 }
