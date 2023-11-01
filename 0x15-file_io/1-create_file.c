c5a36eff286fc556082fa94b6531bd2d5b95e3a6
YoussefAhmed52 <yousef3012001@gmail.com>
2023-11-01 04:19:18 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,6 +31,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
+		return (-1);
+	if (length)
 		b = write(file, text_content, length);
 	close(file);
 	return (b == length ? 1 : -1);
