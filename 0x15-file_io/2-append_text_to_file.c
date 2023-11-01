888c4dfd3e7d3283a8bd39ebf73d3215441683de
YoussefAhmed52 <yousef3012001@gmail.com>
2023-11-01 04:30:34 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,6 +34,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (length)
 		b = write(file, text_content, length);
-	close (file);
+	close(file);
 	return (b == length ? 1 : -1);
 }
