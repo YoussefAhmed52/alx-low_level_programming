f589ee6a038f85ceac8343e779c59977a8e6f9ab
YoussefAhmed52 <yousef3012001@gmail.com>
2023-11-01 04:29:09 -0700

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - length of the string
+ * @s: the string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		j++;
+	return (j);
+}
+/**
+ * create_file - create file
+ * @text_content: text
+ * @filename: file name
+ * Return: 1 on success 0 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t b = 0, length = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	if (length)
+		b = write(file, text_content, length);
+	close (file);
+	return (b == length ? 1 : -1);
+}
