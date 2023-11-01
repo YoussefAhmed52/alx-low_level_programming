7857e2e294bcbe326f4bde6b019b782d1c39972b
YoussefAhmed52 <yousef3012001@gmail.com>
2023-11-01 04:13:04 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - length of the string
+ * @s: the string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		j++;
+	return (j);
+}
+/**
+ * create_file - create file
+ * @text_content: text
+ * @filename: file name
+ * Return: 1 on success 0 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t b = 0, length = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		b = write(file, text_content, length);
+	close(file);
+	return (b == length ? 1 : -1);
+}
